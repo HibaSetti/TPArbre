c9159994776bffc4c49d5fc587463df4bd5a05a8
Hiba <hiba.ouldsetti@gmail.com>
2018-01-06 00:47:20 +0100

    random fct

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,19 @@
+import java.util.Random;
+
 public class Main {
 
     public static void main(String args[])
     {
         Abr a =new Abr(6);
-        a.insertion(5);
-        a.insertion(7);
-        a.insertion(8);
-        a.insertion(3);
-        a.insertion(1);
+        int b = (int) (Math.random() * 10 );
+      for(int i=0;i<10;i++)
+      {
+          a.insertion(b);
+          b = (int) (Math.random() * 10 );
+      }
 
         a.ParcoursInfixe();
-       // System.out.println("la hauteur est :"+ Abr.hauteur(a));
+      System.out.println("la hauteur est :"+ Abr.hauteur(a));
        if( a.recherche(31) == true )
            System.out.println("le nombre existe");
        else
